52b90819f0023efa43706f32c3c1dc854e28f456
Chan Woo Park <14cpark@gmail.com>
2020-02-06 19:12:03 -0800

    define structs, declare functions

--- main.c
+++ main.c
@@ -1,5 +1,71 @@
 #include <stdio.h>
 
+#define PGNUM 8
+#define DISKSIZE 8
+#define MMSIZE 4
+
+struct pt_entry
+{
+    int entry;
+    int valid;
+    int dirty;
+    int pgnum;
+};
+
+struct page
+{
+    int data[4];
+    int originaddr;
+};
+
+struct pt_entry ptable[PGNUM];
+
+struct page diskmem[DISKSIZE];
+
+struct page mainmem[MMSIZE];
+
+int addrtrans(int addr)
+{
+    return addr / 4;
+}
+
+int addrtransoff(int addr)
+{
+    return addr % 4;
+}
+
+//accesses the address and offset in ptable
+//and does relevant calculations.
+void accessptable(int addr, int offset)
+{
+    struct pt_entry * entry = ptable[addr];
+ 
+    //if not valid then access disk
+    if (entry->valid == 0)
+    {
+        //access disk due to page fault
+    
+    }
+    //if valid access main memory
+    else
+    {
+        //access main memory
+    }
+}
+
+//copy index address in disk into main memory
+void copypage(int addr)
+{
+    struct page * pg = diskmem[addr];
+    pg->originaddr = addr;
+
+
+}
+
+
+
+
+
 void parsecmd(char * buf)
 {
     char * cmd;
